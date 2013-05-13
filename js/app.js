ba800e28b7d213b344b9dcf333adfe056289be95
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2013-05-13 14:22:01 +0100

    additional code for tutorial part 3

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ App = Ember.Application.create({
 
 App.Router.map(function() {
   this.resource('posts');
-  this.resource('post', { path: 'posts/:post_id' }, function() {
+  this.resource('post', { path: '/posts/:post_id' }, function() {
+    this.resource('comments', function() {
+  	  this.route('new');
+      this.route('create');
+    });
+    this.route('comment', { path: 'comments/:comment_id'});
   });
 });
 
@@ -20,13 +25,39 @@ App.PostIndexRoute=Ember.Route.extend({
   }
 });
 
+App.CommentsNewRoute=Ember.Route.extend({
+  setupController: function(controller, model) {
+    controller.set('text', null);
+  }	
+});
+
+App.CommentsNewController=Ember.ObjectController.extend({
+	needs: 'post',
+	
+	text: null,
+	
+	save: function() {
+		var post = this.get('controllers.post.content');
+		App.Comment.createRecord({ post: post, text: this.get('text') });
+		this.get('target').transitionTo('post.index');
+	}
+});
+
 App.Store = DS.Store.extend({
   revision: 12,
   adapter: 'DS.FixtureAdapter'
 });
 
 App.Post=DS.Model.extend({
+  comments: DS.hasMany('App.Comment'),
   title: DS.attr('string')
 });
 
-App.Post.FIXTURES=[{ id: "1", title: "First Post" }, { id: "2", title: "Second Post" }, { id: "3", title: "Third Post" }];
+App.Comment = DS.Model.extend({
+  post: DS.belongsTo('App.Post'),
+  text: DS.attr('string')
+});
+
+App.Post.FIXTURES=[{ id: "1", title: "First Post", comments: [1, 2, 3] }, { id: "2", title: "Second Post", comments: [4, 5, 6]  }, { id: "3", title: "Third Post", comments: [7, 8, 9]  }];
+
+App.Comment.FIXTURES = [{id:"1", text: "First Comment"}, {id:"2", text: "Second Comment"}, {id:"3", text: "Third Comment"}, {id:"4", text: "Fourth Comment"}, {id:"5", text: "Fifth Comment"}, {id:"6", text: "Sixth Comment"}, {id:"7", text: "Seventh Comment"}, {id:"8", text: "Eighth Comment"}, {id:"9", text: "Ninth Comment"}];
